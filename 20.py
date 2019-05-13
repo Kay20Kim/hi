16679c55abc33c9f97a4f08bf9996539adc640ee
Kay <45115142+Kay20Kim@users.noreply.github.com>
2019-05-13 21:42:22 +0900

    hi

---
+++ 20.py
@@ -0,0 +1,40 @@
+A = [70,60,55,75,95,80,80,85,100]
+sum =0
+for i in A:
+    sum +=i
+sum /= len(A)
+print(sum)
+
+'''
+str = input()
+strSplit = str.split()
+a = int(strSplit[0])
+b = 
+'''
+'''
+N = int(input())
+bag = 0
+
+while True:
+    if N % 5 == 0:
+        bag += (N//5)
+        print(bag)
+        break
+    N -= 3
+    bag += 1
+    if N <0 :
+        print(-1)
+        break
+    
+'''
+
+
+'''
+
+while True:
+    try:
+        x=input()
+        print(x)
+    except:
+        break
+'''
